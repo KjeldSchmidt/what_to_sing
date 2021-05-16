d795efe140dc4c06e013c84b0115d9a70f631cf6
Kjeld Schmidt <contact@kjeld-schmidt.com>
2021-05-16 20:27:00 +0200

    Added landing page claim

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -2,6 +2,8 @@ import AuthorizeButton from "../components/AuthorizeButton";
 import React from "react";
 import {RouteComponentProps, useLocation, withRouter} from "react-router-dom";
 import {WithStyles, withStyles} from "@material-ui/core";
+import karafunLogo from '../assets/karafunLogo.png';
+import spotifyLogo from '../assets/spotifyLogo.png';
 
 const styles = {
     message: {
@@ -17,6 +19,24 @@ const styles = {
     buttonWrapper: {
         textAlign: "center" as const
     },
+    claim: {
+        textAlign: "center" as const,
+        fontSize: "3rem",
+        margin: "2rem",
+
+        "& img": {
+            height: "5rem",
+            width: "auto",
+            verticalAlign: "middle",
+            marginLeft: "1rem",
+            marginRight: "1rem",
+        },
+
+        "& p": {
+            margin: "0.3rem"
+        }
+    }
+}
 
 interface LandingPageProps extends WithStyles<typeof styles>, RouteComponentProps {}
 
@@ -29,6 +49,16 @@ function LandingPage(props: LandingPageProps) {
     const { classes } = props;
     return (
         <div className={classes.container}>
+            <div className={classes.claim}>
+                <p>
+                    Find your songs from
+                    <img src={spotifyLogo} alt="Spotify"/>
+                </p>
+                <p>
+                    at
+                    <img src={karafunLogo} alt="Karafun"/>
+                </p>
+            </div>
             { state?.landingMessage &&
                 <h4 className={classes.message}>
                     {state.landingMessage}
